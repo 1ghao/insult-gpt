34e1964e6af8bd1300aa0c54ccbfb89935b39f13
1ghao <linwanghao2007@gmail.com>
2025-02-04 20:01:14 +0100

    github pages test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: false,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  distDir: "dist",
 };
 
 export default nextConfig;
